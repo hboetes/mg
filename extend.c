87e2687596190f9b06efe386b291b9acae065162
Han Boetes <han@boetes.org>
2021-12-12 08:41:32 +0100

    kill warning: ‘curmap’ may be used uninitialized in this function

--- extend.c
+++ extend.c
@@ -695,7 +695,7 @@ excline(char *line, int llen, int lnum)
 	char	*argp = NULL;
 	long	 nl;
 	int	 bind;
-	KEYMAP	*curmap;
+	KEYMAP	*curmap = NULL;
 #define BINDARG		0  /* this arg is key to bind (local/global set key) */
 #define	BINDNO		1  /* not binding or non-quoted BINDARG */
 #define BINDNEXT	2  /* next arg " (define-key) */
