da4d6ce728529f1fa44a71673a0495b9b9c3c661
Han Boetes <hboetes@gmail.com>
2026-03-23 21:18:12 +0100

    dirty hack to fix building with alpine/musl
    Yes, I could add a bunch of code to fix this problem. But this works
    well enough.

--- compat.h
+++ compat.h
@@ -1,3 +1,10 @@
 #ifdef __FreeBSD__
 void *recallocarray(void *, size_t, size_t, size_t);
 #endif
+
+/* REG_STARTEND is not available in musl libc. This stub allows compilation but
+ * regex searches will not honor start offsets, causing forward search to find
+ * matches before the cursor position. */
+#ifndef REG_STARTEND
+#define REG_STARTEND 0
+#endif
